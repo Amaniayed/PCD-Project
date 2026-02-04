d0eebd0a585b97ae487e00f69050a40a22058f15
majda <soussiamajda@gmail.com>
2026-02-04 21:24:57 +0100

    generate a full years dataset

--- src/visualize_master.py
+++ src/visualize_master.py
@@ -4,31 +4,26 @@ from day_manager_simulator import MasterDayManager
 
 def visualize_each_scenario():
     manager = MasterDayManager()
-    
-    # Dictionnaire complet des 20 scénarios
+
     scenarios = {
-        1: "Daily Routine", 2: "Routine + Laundry", 3: "Routine + Light Cleaning",
-        4: "Laundry + Cleaning", 5: "Routine + Cooking", 6: "Laundry + Cooking + Guests",
-        7: "Social Day", 8: "Big Cleaning", 9: "Outside Day", 10: "Extreme Cold Day",
-        11: "Radio Fan Day", 12: "Winter Tea Day", 13: "Weekly Laundry",
-        14: "Grandchildren Visit", 15: "Small Maintenance", 16: "Early Bird Morning",
-        17: "Traditional Lunch Cooking", 18: "Computer Hobby Session", 19: "Monthly Steam Cleaning",
-        20: "Rest and Sick Day"
+        1: "Daily Routine",
+        2: "Routine + Laundry",
+        3: "Routine + Cleaning",
+        4: "Routine + Cooking",
+        5: "Outside Day",
     }
 
-    # Création du dossier de destination
-    output_dir = "reports/scenarios_individuels"
+    output_dir = "reports/figures"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
         print(f"Dossier créé : {output_dir}")
 
-    print("Génération des graphiques individuels...")
+    print("Generation of individual graphs...")
 
     for sid, name in scenarios.items():
         # Générer les données
-        data = manager.generate_day(sid)
-        minutes = list(data.keys())
-        watts = list(data.values())
+        watts = manager.generate_day(sid)
+        minutes = list(range(1, 1441))
 
         # Création de la figure
         plt.figure(figsize=(12, 6))
@@ -40,16 +35,16 @@ def visualize_each_scenario():
         plt.fill_between(minutes, watts, color='tab:blue', alpha=0.2)
 
         # Configuration des axes et titres
-        plt.title(f"Scénario {sid} : {name}", fontsize=16, fontweight='bold')
-        plt.xlabel("Temps (Minutes de 1 à 1440)", fontsize=12)
-        plt.ylabel("Puissance (Watts)", fontsize=12)
+        plt.title(f"scenario {sid} : {name}", fontsize=16, fontweight='bold')
+        plt.xlabel("Time (Minutes from 1 to 1440)", fontsize=12)
+        plt.ylabel("Power (watts)", fontsize=12)
         plt.grid(True, linestyle='--', alpha=0.5)
         plt.xlim(1, 1440)
         plt.ylim(0, 5500) # Garder la même échelle pour comparer facilement les fichiers
 
         # Ajout d'une zone grise pour la nuit (Sommeil : 22h-6h)
         # 0 à 360 min (0h-6h) et 1320 à 1440 min (22h-0h)
-        plt.axvspan(1, 360, color='gray', alpha=0.1, label='Nuit (Sommeil)')
+        plt.axvspan(1, 360, color='gray', alpha=0.1, label='Night (Sleep)')
         plt.axvspan(1320, 1440, color='gray', alpha=0.1)
 
         plt.legend()
